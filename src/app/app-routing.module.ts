85f932bcd6e26a18c46f38645d9c8047531c554e
LuanBui02 <luanblp@teamsolutions.vn>
2024-03-12 11:22:52 +0700

    update imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductsComponent} from "./container/products/products.component";
+import {CartComponent} from "./container/cart/cart.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'products', pathMatch:"full"},
+  {path: 'products', component: ProductsComponent},
+  {path: 'cart', component: CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CartComponent} from "./container/cart/cart.component";
+import {MenuComponent} from "./container/menu/menu.component";
+import {ProductsComponent} from "./container/products/products.component";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    MenuComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
