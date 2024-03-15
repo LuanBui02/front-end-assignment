51f875734c6e9079ffdf268f0c1c24c4111c25f2
LuanBui02 <luanblp@teamsolutions.vn>
2024-03-15 12:59:34 +0700

    update productsComponent

--- src/app/container/products/products.component.ts
+++ src/app/container/products/products.component.ts
@@ -9,17 +9,35 @@ import {CartService} from "../../service/cart.service";
 })
 export class ProductsComponent implements OnInit {
   public productList: any;
+  public filterCategory: any;
+  public searchKey:string = "";
   constructor(private api: ApiService, private cartService: CartService) { }
 
   ngOnInit(): void {
     this.api.getProducts().subscribe(res => {
       this.productList = res;
+      this.filterCategory = res;
       this.productList.forEach((a:any) => {
-        Object.assign(a, {quantity: a.quantity,total: a.total});
+        Object.assign(a, {quantity: 1,total: a.total, date: new Date()});
       });
     });
+
+    this.cartService.search.subscribe((val:any)=>{
+      this.searchKey = val;
+    })
   }
   addToCart(product:any) {
-    this.cartService.addToCart(product);
+    if(!this.cartService.productInCart(product)) {
+      this.cartService.addToCart(product);
+    }
   }
+  filter(category:string){
+    this.filterCategory = this.productList
+      .filter((a:any)=>{
+        if(a.category == category || category==''){
+          return a;
+        }
+      })
+  }
+
 }
