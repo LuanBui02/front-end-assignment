83bbbbf7ab216c598b3d8f4da8fc19be65c0a36b
LuanBui02 <luanblp@teamsolutions.vn>
2024-03-16 17:57:34 +0700

    add admin-orderComponent

---
+++ src/app/container/admin-order/admin-order.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {CartService} from "../../service/cart.service";
+
+@Component({
+  selector: 'app-admin-order',
+  templateUrl: './admin-order.component.html',
+  styleUrls: ['./admin-order.component.scss']
+})
+export class AdminOrderComponent implements OnInit {
+  public products: any;
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.getProduct()
+      .subscribe((res) => {
+        this.products = res;
+      })
+    this.cartService.loadCart();
+    this.products = this.cartService.getProductInCart();
+
+  }
+  totalSum(): number {
+    return this.products.reduce((sum: any, product: any) => ({
+      quantity: 1,
+      price: sum.price + product.quantity * product.price,
+    })).price;
+
+  }
+
+}
