8e736204dfb00daba7ec38ff0e0ad52e28d3ba87
LuanBui02 <luanblp@teamsolutions.vn>
2024-03-20 12:04:39 +0700

    update admin-item.component.

--- src/app/component/admin/admin-item/admin-item.component.ts
+++ src/app/component/admin/admin-item/admin-item.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AdminItemService} from "../../../service/admin-item.service";
 import {FormBuilder, Validators} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-admin-item',
@@ -11,9 +12,9 @@ export class AdminItemComponent implements OnInit {
   public products: any;
   public messageSuccess !: string;
   public submitted = false;
-  public messageFail !: string;
-
-  constructor(private adminService: AdminItemService, private formBuilder: FormBuilder) {
+  public check = null;
+  public messageError !: string;
+  constructor(private adminService: AdminItemService, private formBuilder: FormBuilder, private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
@@ -26,12 +27,22 @@ export class AdminItemComponent implements OnInit {
   submit(product: any) {
     this.submitted = true;
     this.adminService.addProduct(product).subscribe(
-      (response) => {
+      (response: any) => {
+        if(!response) {
+          this.messageSuccess = "Item is added"
+        }
+        console.log(response);
         alert(response.message);
       },
       (error) => {
-        console.log(error);
-      });
+        alert(error.error.message);
+        console.log(product.price);
+        this.check = product.price;
+        console.log(this.check);
+        if(this.check == 0) {
+          this.messageError = error.error.message;
+        }
+      })
   }
 }
 
