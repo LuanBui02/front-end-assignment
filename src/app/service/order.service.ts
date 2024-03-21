81b0af16191ad554fd89396a8914f11ba2e1362d
LuanBui02 <luanblp@teamsolutions.vn>
2024-03-21 11:29:11 +0700

    update orderService

--- src/app/service/order.service.ts
+++ src/app/service/order.service.ts
@@ -1,16 +1,14 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {BehaviorSubject, map} from "rxjs";
 import {CartService} from "./cart.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
-  constructor(private cartService: CartService, private http: HttpClient) {
+  constructor(private cartService: CartService) {
   }
 
-  addToOrder(cart: any) {
-    this.cartService.addToCart(cart);
+  addToOrder() {
+    this.cartService.addToOrder();
   }
 }
