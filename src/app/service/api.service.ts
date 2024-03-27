7848320ed84634c515281e31b50ac5252f7148fb
LuanBui02 <luanblp@teamsolutions.vn>
2024-03-27 22:39:27 +0700

    update apiService

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,20 +1,34 @@
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {map} from "rxjs";
+import {Router} from "@angular/router";
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  public isLoginError = new EventEmitter<boolean>(false);
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
   }
 
-  getProducts() {
-    return this.http.get<any>("http://localhost:8080/api/items")
-      .pipe(map((res: any) => {
-        return res;
-      }))
+  userLogin(data: any) {
+    return this.http.get(
+      `http://localhost:8080/api/login/user/username/password?username=${data.username}&password=${data.password}`,
+      {observe: 'response'})
+  }
+
+  adminLogin(data: any) {
+    return this.http.get(
+      `http://localhost:8080/api/login/admin/username/password?username=${data.username}&password=${data.password}`,
+      {observe: 'response'})
+  }
+  addAccountUser(data:any) {
+    return this.http.post(
+      `http://localhost:8080/api/login/user`, data);
+  }
+  addAccountAdmin(data:any) {
+    return this.http.post(
+      `http://localhost:8080/api/login/admin`, data);
   }
 }
